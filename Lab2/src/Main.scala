bc98fe6ea48d323bd1ceb08a8b3446c0c7139cda
Viktor Claesson <claesson.viktor@gmail.com>
2017-03-28 12:02:00 +0200

    removed time stamping

--- Lab2/src/Main.scala
+++ Lab2/src/Main.scala
@@ -8,27 +8,18 @@ import scala.io.Source
 object Main {
   def main(args: Array[String]): Unit = {
     // time0 is for measuring the time it takes to perform the building of tree.
-    var time0: Long = 0
-    time0 = System.currentTimeMillis
     val nodes = Source.fromFile("data/words-50.txt").getLines.map(x => new Node(x)).toVector
-    println("Reading file: " + (System.currentTimeMillis - time0) + " ms")
     val map: Map[String, ArrayBuffer[Node]] = Map.empty[String, ArrayBuffer[Node]]
 
     // We have a word, we get all possible substrings that are 1 character shorter & sorted.
     // Then we add them to the map, were there are two choices.
     //   1: Keyword doesn't exist, so we add a new entry mapping (keyword -> ArrayBuffer(word))
     //   2: Keyword does exist, so we get the ArrayBuffer for the keyword already in the map, and append to it the word.
-    time0 = System.currentTimeMillis
     nodes.foreach(node => allOneShorterSubstrings(node.toString).foreach(key => addToMap(map, key, node)))
-    println("Build map: " + (System.currentTimeMillis - time0) + " ms")
-    time0 = System.currentTimeMillis
     nodes.foreach(node => node.setAdjacencyString(t4(map, node)))
-    println("Set adjacency: " + (System.currentTimeMillis - time0) + " ms")
-
+    
     // Printing the list, mostly for checking validity
-    time0 = System.currentTimeMillis
     nodes.foreach(n => println(n.adjacencyString))
-    println("Printing List: " + (System.currentTimeMillis - time0) + " ms")
   }
 
   def allOneShorterSubstrings(arg: String): Set[String] = {
