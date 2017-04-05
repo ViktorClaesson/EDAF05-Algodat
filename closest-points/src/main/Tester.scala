b65e9e4e4491febe2840f64995ae0bce7d08a60b
Sebastian Hegardt <se.hegardt@gmail.com>
2017-04-05 08:50:47 +0200

    Fixed testing output

--- closest-points/src/main/Tester.scala
+++ closest-points/src/main/Tester.scala
@@ -5,6 +5,8 @@ import java.io.File
 import point.Point
 import reader.FileReader
 
+import scala.io.Source
+
 /**
   * Created by Sebastian on 04/04/2017.
   */
@@ -13,7 +15,14 @@ object Tester {
   //Find all filenames
   val d = new File("data")
   val filenames = d.listFiles.map(f => f.toString).filterNot(name => name.contains("README") || name.contains("closest-pair-out")).toVector
-  val outs = 
+  val outs: Map[String, String] = Source.fromFile("data/closest-pair-out.txt").getLines().map(l => {
+    val li = l.split(" ");
+    li(0).drop(8).dropRight(5) -> li(2)
+  }).toMap
+
+  def printAns(): Unit ={
+    outs.foreach(println)
+  }
 
   def printFilenames(): Unit ={
     filenames.foreach(println)
@@ -21,23 +30,18 @@ object Tester {
 
   def testAll(): Unit ={
     for(name <- filenames){
-      if(name.contains("close") || name.contains("wc-instance")){
-        val (p1, p2) = Algorithm.closest_pair(FileReader.read_close(name))
-        print(name + ": "); println(s"${p1.name} -> ${p2.name}: ${p1.dist(p2)}")
-      }else{
-        val (p1, p2) = Algorithm.closest_pair(FileReader.read_other(name))
-        print(name + ": "); println(s"${p1.name} -> ${p2.name}: ${p1.dist(p2)}")
-      }
+      testOne(name)
     }
   }
 
   def testOne(name: String): Unit ={
+    val ans = outs.get(name.drop(5).dropRight(8))
     if(name.contains("close") || name.contains("wc-instance")){
       val (p1, p2) = Algorithm.closest_pair(FileReader.read_close(name))
-      print(name + ": "); println(s"${p1.name} -> ${p2.name}: ${p1.dist(p2)}")
+      print(name + ": "); println(s"${p1.name} -> ${p2.name}: ${p1.dist(p2)}, Should be: N/A")
     }else{
       val (p1, p2) = Algorithm.closest_pair(FileReader.read_other(name))
-      print(name + ": "); println(s"${p1.name} -> ${p2.name}: ${p1.dist(p2)}")
+      print(name + ": "); println(s"${p1.name} -> ${p2.name}: ${p1.dist(p2)}, Should be: ${ans.get}")
     }
   }
 
