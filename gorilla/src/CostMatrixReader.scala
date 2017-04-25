8881ff03f089e7aaf7941617938eb0e646edd4f3
Sebastian Hegardt <se.hegardt@gmail.com>
2017-04-25 15:32:10 +0200

    Finished readMatrix

--- gorilla/src/CostMatrixReader.scala
+++ gorilla/src/CostMatrixReader.scala
@@ -1,7 +1,16 @@
 /**
   * Created by Sebastian on 25/04/2017.
   */
+
+import scala.io.Source
+
 object CostMatrixReader {
 
-  
+  val costMatrix = "data/BLOSUM62.txt"
+
+  def readMatrix(file: String): Vector[Vector[Int]] = {
+    val header = Source.fromFile(file).getLines().drop(6).take(1).toString().split(" ")
+    val matrix: Vector[Vector[Int]] = Source.fromFile(file).getLines().drop(7).map(l => l.split("\\s+").toVector.tail.map(_.trim.toInt)).toVector
+    matrix
+  }
 }
---
+++ gorilla/src/Main.scala
@@ -0,0 +1,9 @@
+/**
+  * Created by Sebastian on 25/04/2017.
+  */
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    CostMatrixReader.readMatrix(CostMatrixReader.costMatrix)
+  }
+}
