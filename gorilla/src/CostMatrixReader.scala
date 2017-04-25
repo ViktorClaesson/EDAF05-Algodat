c5095c1a2a45c82e9a8138d1f4b8242ead687a34
Viktor Claesson <claesson.viktor@gmail.com>
2017-04-25 15:42:44 +0200

    added stuff

--- gorilla/src/CostMatrixReader.scala
+++ gorilla/src/CostMatrixReader.scala
@@ -1,7 +1,16 @@
+import scala.io.Source
+
 /**
   * Created by Sebastian on 25/04/2017.
   */
 object CostMatrixReader {
 
-  
+  val order = List('A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V', 'B', 'Z', 'X', '*');
+
+  def getIndex(c: Char) = (order.indexOf(c) + order.length) % order.length
+
+  def readMatrix2(file: String): Array[Array[Int]] = {
+    Source.fromFile(file).getLines().drop(7).map(arr => arr.drop(1).trim.split("\\s+")).map(_.map(_.toInt)).toArray;
+  }
+
 }
---
+++ gorilla/src/Main.scala
@@ -0,0 +1,10 @@
+/**
+  * Created by viktor on 2017-04-25.
+  */
+object Main {
+
+  def main(args: Array[String]) = {
+    val arr = CostMatrixReader.readMatrix2("data/BLOSUM62.txt")
+  }
+
+}
