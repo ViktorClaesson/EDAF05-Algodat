e4e7fe818884aa4b1655803cddb01e32cb168fe1
Viktor Claesson <claesson.viktor@gmail.com>
2017-04-26 08:15:36 +0200

    building Strings

--- gorilla/src/Alignment.scala
+++ gorilla/src/Alignment.scala
@@ -3,36 +3,65 @@ import scala.collection.mutable.ArrayBuffer
 /**
   * Created by Sebastian on 25/04/2017.
   */
-class Alignment (val str1: String, val str2: String){
+class Alignment(val str1: String, val str2: String) {
 
   val memoryMatrix = MemoryMatrix.createMatrix(str1, str2)
-  var aligned1: String = ""
-  var aligned2: String = ""
+  var done: Boolean = false
 
-  def alignment(costMatrix: Vector[Vector[Int]]): Int ={
-    for(j <- 1 until memoryMatrix(0).length; i <- 1 until memoryMatrix.length){
+  def alignment(costMatrix: Vector[Vector[Int]]): Int = {
+    if (done)
+      return memoryMatrix.last.last
+
+    for (j <- 1 until memoryMatrix(0).length; i <- 1 until memoryMatrix.length) {
       memoryMatrix(i)(j) = opt(i, j, costMatrix)
     }
-
-    memoryMatrix.last.last + getCost(str1.last, str2.last, costMatrix)
+    memoryMatrix(memoryMatrix.length - 1)(memoryMatrix(0).length - 1) += getCost(str1.last, str2.last, costMatrix)
+    done = true
+    memoryMatrix.last.last
   }
 
-  def opt(i: Int, j: Int, costMatrix: Vector[Vector[Int]]): Int ={
-    val cost = CostMatrixReader.emptyCost
-    val max = Vector(getCost(str1(i-1), str2(j-1), costMatrix) + memoryMatrix(i-1)(j-1), cost + memoryMatrix(i-1)(j), cost + memoryMatrix(i)(j-1)).zipWithIndex.max
+  def buildWords(costMatrix: Vector[Vector[Int]]): (String, String) = {
+    bind(buildWords(memoryMatrix.length - 1, memoryMatrix(0).length - 1, costMatrix), (str1.last.toString, str2.last.toString))
+  }
 
-    max match {
-      case (v, 0) => aligned1 += str1(i-1); aligned2 += str2(j-1); return v
-      case (v, 1) => aligned2 += '_'; return v
-      case default => aligned1 += '_'; return max._1
+  private def buildWords(i: Int, j: Int, costMatrix: Vector[Vector[Int]]): (String, String) = {
+    if(i == 0 && j == 0)
+      return ("", "")
+
+    val max = getAlternatives(i, j, costMatrix).zipWithIndex.max
+    max._2 match {
+      case 0 => return bind(buildWords(i - 1, j - 1, costMatrix), (str1(i - 1).toString, str2(j - 1).toString))
+      case 1 => return bind(buildWords(i - 1, j, costMatrix), (str1(i - 1).toString, "_"))
+      case default => return bind(buildWords(i, j - 1, costMatrix), ("_", str2(j - 1).toString))
     }
   }
 
-  def getCost(c1: Char, c2: Char, costMatrix: Vector[Vector[Int]]): Int ={
+  def bind(t0: (String, String), t1: (String, String)): (String, String) = {
+    (t0._1 + t1._1, t0._2 + t1._2)
+  }
+
+  def matrixValue(i: Int, j: Int): Int = {
+    if (i < 0 || j < 0)
+      return Integer.MIN_VALUE
+    else
+      return memoryMatrix(i)(j)
+  }
+
+  def getAlternatives(i: Int, j: Int, costMatrix: Vector[Vector[Int]]): Vector[Int] = {
+    val cost = CostMatrixReader.emptyCost
+    Vector(getCost(str1(i - 1), str2(j - 1), costMatrix) + matrixValue(i - 1, j - 1), cost + matrixValue(i - 1, j), cost + matrixValue(i, j - 1))
+  }
+
+  def opt(i: Int, j: Int, costMatrix: Vector[Vector[Int]]): Int = {
+    val max = getAlternatives(i, j, costMatrix).max
+    max
+  }
+
+  def getCost(c1: Char, c2: Char, costMatrix: Vector[Vector[Int]]): Int = {
     costMatrix(CostMatrixReader.getIndex(c1))(CostMatrixReader.getIndex(c2))
   }
 
-  def printMemoryMatrix(): Unit ={
+  def printMemoryMatrix(): Unit = {
     memoryMatrix.foreach(l => println(l.mkString(", ")))
   }
 }
--- gorilla/src/Main.scala
+++ gorilla/src/Main.scala
@@ -8,12 +8,10 @@ object Main {
     val costMatrix = CostMatrixReader.readMatrix("data/BLOSUM62.txt")
     //costMatrix.foreach(l => println(l.mkString(", ")))
 
-    val al = new Alignment("KQRK", "KAK")
+    val al = new Alignment("KAK", "KQRIKAAKABK")
     val cost = al.alignment(CostMatrixReader.readMatrix("data/BLOSUM62.txt"))
-    al.printMemoryMatrix()
-
-    println(cost)
-
-    println(al.aligned1 + "\n" + al.aligned2)
+    cost
+    val aligned = al.buildWords(costMatrix)
+    println(aligned._1 + "\n" + aligned._2)
   }
 }
