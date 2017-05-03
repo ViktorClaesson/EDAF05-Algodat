ab6ae012aa9f20c2338c5c791aa4e3ac4da0bcc4
viktorclaesson <claesson.viktor@gmail.com>
2017-05-03 16:21:33 +0200

    h

--- flow/src/FordFulkerson.scala
+++ flow/src/FordFulkerson.scala
@@ -9,33 +9,33 @@ import scala.collection.mutable.Queue
 object FordFulkerson {
 
   def run(s: Node, t: Node): Unit ={
-    while(BFS.build(s, t)) {
-      val path = new Path(s, t)
-      path.updateResidualCapacity()
-    }
+    while(BFS.build(s, t)) {                                            // While it finds a path from start to end
+      val path = new Path(s, t)                                         // Create path from start to end
+      path.updateResidualCapacity()                                     // Update all the residual capacity
+    }                                                                   // if done it can't create another path from start to end
 
-    val minCut = minimalCut(s)
-    val edgeEdges = minCut.map(n => n.adjacencyList).flatten.distinct
-      .filter(e => e.residualCapacity == 0)
-      .filterNot(e => minCut.contains(e.terminalNode))
-    edgeEdges.sortBy(_.startingNode.index).foreach(println)
-    printf("Sum: %d\n", edgeEdges.map(e => e.capacity).sum)
+    val minCut = minimalCut(s)                                          // Creates Vector[Node] consisting of all the Nodes that are part of the minimal cut
+    val edgeEdges = minCut.flatMap(n => n.adjacencyList).distinct       // Adds all the edges from the nodes to a vector
+      .filter(e => e.residualCapacity == 0)                             // Filter only the ones with max flow
+      .filterNot(e => minCut.contains(e.terminalNode))                  // Filter only the ones with other part of the edge not in the mincut set
+    edgeEdges.sortBy(_.startingNode.index).foreach(println)             // Just sorting for nicer output and printing all the edges
+    printf("Sum: %d\n", edgeEdges.map(e => e.capacity).sum)             // Prints the sum
   }
 
   // Returns a Vector of all the nodes that are in the minimal cut
   def minimalCut(root: Node): Vector[Node] = {
-    val visitedNodes = ArrayBuffer(root)
-    val queue = Queue(root)
+    val visitedNodes = ArrayBuffer(root)                                // Creates a buffer of all visited nodes (beginning with root)
+    val queue = Queue(root)                                             // Creates a queue with nodes that we visit (beginning with root)
 
     do {
-      val current = queue.dequeue()
-      val edges = current.adjacencyList
-        .filterNot(e => e.residualCapacity == 0)
-        .filterNot(e => visitedNodes.contains(e.terminalNode))
-      visitedNodes ++= edges.map(e => e.terminalNode)
-      edges.foreach(e => queue.enqueue(e.terminalNode))
+      val current = queue.dequeue()                                     // Gets the first node in the queue
+      val edges = current.adjacencyList                                 // Get all the edges from the node
+        .filterNot(e => e.residualCapacity == 0)                        // Filter out the ones with max flow
+        .filterNot(e => visitedNodes.contains(e.terminalNode))          // Filter out already visited nodes
+      visitedNodes ++= edges.map(e => e.terminalNode)                   // Add nodes to visited
+      edges.foreach(e => queue.enqueue(e.terminalNode))                 // Add nodes to queue
     } while (!queue.isEmpty)
 
-    visitedNodes.toVector
+    visitedNodes.toVector                                               // Return the visited nodes aka the minimal cut
   }
 }
